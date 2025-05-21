49c476a1db9299432b305dc64357da6823f04628
lalilu <1248393538@qq.com>
2025-05-21 21:42:53 +0800

    refactor(main): 重构主程序并初始化 UI
    - 移除了 lv_example_button_1 函数和相关的事件处理代码
    - 添加了 ui.h 头文件引用
    - 在 setup 函数中调用了 ui_init 和 lv_screen_load 函数- 增加了文件系统大小信息的打印

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include "FS.h"
 #include <LittleFS.h>
 #include "lv_port_disp_m5.hpp"
+#include "ui.h"
 
 uint32_t my_tick_get_cb() {
     return esp_timer_get_time() / 1000;
@@ -20,39 +21,6 @@ void my_input_read_cb(lv_indev_t *indev, lv_indev_data_t *data) {
         data->point.x = touch.x;
         data->point.y = touch.y;
     }
-}
-
-static void event_handler(lv_event_t *e) {
-    const lv_event_code_t code = lv_event_get_code(e);
-
-    if (code == LV_EVENT_CLICKED) {
-        Serial.println("Clicked");
-    } else if (code == LV_EVENT_VALUE_CHANGED) {
-        Serial.println("Toggled");
-    }
-}
-
-void lv_example_button_1() {
-    lv_obj_t *label;
-
-    lv_obj_t *btn1 = lv_button_create(lv_screen_active());
-    lv_obj_add_event_cb(btn1, event_handler, LV_EVENT_ALL, NULL);
-    lv_obj_align(btn1, LV_ALIGN_CENTER, 0, -40);
-    lv_obj_remove_flag(btn1, LV_OBJ_FLAG_PRESS_LOCK);
-
-    label = lv_label_create(btn1);
-    lv_label_set_text(label, "Button");
-    lv_obj_center(label);
-
-    lv_obj_t *btn2 = lv_button_create(lv_screen_active());
-    lv_obj_add_event_cb(btn2, event_handler, LV_EVENT_ALL, NULL);
-    lv_obj_align(btn2, LV_ALIGN_CENTER, 0, 40);
-    lv_obj_add_flag(btn2, LV_OBJ_FLAG_CHECKABLE);
-    lv_obj_set_height(btn2, LV_SIZE_CONTENT);
-
-    label = lv_label_create(btn2);
-    lv_label_set_text(label, "Toggle");
-    lv_obj_center(label);
 }
 
 void setup() {
@@ -78,9 +46,12 @@ void setup() {
     Serial.printf("PSRAM size: %d\n", esp_spiram_get_size());
     Serial.printf("Heap free size: %d\n", heap_caps_get_free_size(MALLOC_CAP_8BIT));
     Serial.printf("Heap total size: %d\n", heap_caps_get_total_size(MALLOC_CAP_8BIT));
+    Serial.printf("Fs total size: %d\n", LittleFS.totalBytes());
+    Serial.printf("Fs used size: %d\n", LittleFS.usedBytes());
 
     Serial.println("Start drawing!");
-    lv_example_button_1();
+    ui_init("/");
+    lv_screen_load(home_create());
 }
 
 void loop() {
