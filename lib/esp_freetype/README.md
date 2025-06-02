1cefef98985cd8bd04a2fa03037bcc942570f3fe
lalilu <1248393538@qq.com>
2025-06-03 03:24:57 +0800

    refactor(freetype): 引入esp_freetype库

---
+++ lib/esp_freetype/README.md
@@ -0,0 +1,15 @@
+# Overview
+
+[esp_freetype](https://github.com/100askTeam/esp_freetype) is an out of the box [ESP-IDF component](https://docs.espressif.com/projects/esp-idf/en/latest/esp32/api-guides/build-system.html#component-cmakelists-files) that is as convenient and simple as regular ESP-IDF components, and you can import it into your project for use.
+
+# Examples
+
+You can obtain corresponding usage examples in the example directory.
+
+Please note this before use:
+
+![note](https://forum.lvgl.io/uploads/default/original/2X/6/66b04f421b4a899ca7c3e0c35d231b9d4c1ca6de.png)
+
+# Forum
+
+[https://forums.100ask.net](https://forums.100ask.net)
--- lib/esp_freetype/example/esp_lv_example_freetype.h
+++ lib/esp_freetype/example/esp_lv_example_freetype.h
@@ -1,9 +1,10 @@
 /**
- * @file lvgl_ui.h
+ * @file esp_lv_example_freetype.h
+ *
  */
 
-#ifndef LVGL_UI_H
-#define LVGL_UI_H
+#ifndef ESP_LV_EAMPLE_FREETYPE_H
+#define ESP_LV_EAMPLE_FREETYPE_H
 
 #ifdef __cplusplus
 extern "C" {
@@ -12,7 +13,9 @@ extern "C" {
 /*********************
  *      INCLUDES
  *********************/
-#include "lvgl_ui_gen.h"
+
+//#ifdef CONFIG_ESP_FREETYPE_EXAMPLE_LVGL
+#if 1
 
 /*********************
  *      DEFINES
@@ -23,24 +26,18 @@ extern "C" {
  **********************/
 
 /**********************
- * GLOBAL VARIABLES
- **********************/
-
-/**********************
  * GLOBAL PROTOTYPES
  **********************/
-
-/**
- * Initialize the component library
- */
-void lvgl_ui_init(const char * asset_path);
+void esp_lv_example_freetype(void);
 
 /**********************
  *      MACROS
  **********************/
 
+#endif /*CONFIG_ESP_FREETYPE_EXAMPLE_LVGL*/
+
 #ifdef __cplusplus
 } /*extern "C"*/
 #endif
 
-#endif /*LVGL_UI_H*/
+#endif /*ESP_LV_EAMPLE_FREETYPE_H*/
