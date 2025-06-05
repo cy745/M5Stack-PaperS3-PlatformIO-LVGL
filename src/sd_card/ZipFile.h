8583ad9002425b24926711b72e04a6c05ddffe39
lalilu <1248393538@qq.com>
2025-06-05 17:48:42 +0800

    feat(sd_card): 添加 ZipFile 类用于处理 ZIP 文件
    - 新增 ZipFile 类，提供 ZIP 文件读取功能
    - 实现 read_file_to_mem 方法，将 ZIP 文件中的内容读取到内存
    - 实现 read_file_to_file 方法，将 ZIP 文件中的内容提取到指定文件
    - 依赖 miniz 库进行 ZIP 文件操作

---
+++ src/sd_card/ZipFile.h
@@ -0,0 +1,27 @@
+//
+// Created by miku on 25-6-3.
+//
+
+#ifndef ZIPFILE_H
+#define ZIPFILE_H
+#include <string>
+
+class ZipFile {
+    const char *source_path;
+
+public:
+    explicit ZipFile(const char *source_path) {
+        this->source_path = source_path;
+    }
+
+    ~ZipFile() {
+    }
+
+    // read a file from the zip file allocating the required memory for the data
+    uint8_t *read_file_to_mem(const char *path_inside, size_t *size = nullptr) const;
+
+    bool read_file_to_file(const char *filename, const char *dest) const;
+};
+
+
+#endif //ZIPFILE_H
